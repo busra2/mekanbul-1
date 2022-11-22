637027d464b473b8000d697c93286dc4c30cddd0
busra2 <busra.kemter@outlook.com>
2022-11-22 17:47:01 +0300

    odev5

---
+++ app_api/models/db.js
@@ -0,0 +1,26 @@
+var mongoose = require( 'mongoose');
+require("./mekansema");
+var dbURI = 'mongodb://localhost/mekanbul';
+//var dbURI = 'mongodb+srv://busra_1:busra_1@mekanbul-1.xmudo9o.mongodb.net/mekanbul?retryWrites=true&w=majority';
+mongoose.connect(dbURI);
+function kapat(msg, callback) {
+  mongoose.connection.close(function () {
+    console.log(msg);
+    callback();
+  });
+}
+process.on("SIGINT", function (){
+  kapat("uygulama kapatıldı!", function(){
+    process.exit(0);
+  });
+});
+
+mongoose.connection.on("connected" , function ( ){
+    console.log(dbURI + " adresindeki veritabanına bağlanıldı!\n");
+});
+mongoose.connection.on("error" , function ( ){
+    console.log(dbURI + " bağlantı hatasıdır!\n");
+});
+mongoose.connection.on("disconnected" , function ( ){
+    console.log(dbURI + " bağlantı kesildi !\n");
+});
