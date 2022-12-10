2f85329c6c8b8e4ca7acb504b866ded419565847
busra2 <busra.kemter@outlook.com>
2022-12-10 16:36:35 +0300

    odev8/upgrade3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const axios = require("axios");
 
 var apiSecenekleri = {
-  sunucu:"https://mekanbul-1-1.busrakemter.repl.co",
+  sunucu:"https://mekanbul-1-2.busrakemter.repl.co",
   apiYolu:"/api/mekanlar/",
 };
 var mesafeyiFormatla = function(mesafe){
